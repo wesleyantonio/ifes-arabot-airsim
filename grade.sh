d8ed8785dadd1cb71f9f4d05de166de3abe1d10a
Avelino Forechi <avelino@lcad.inf.ufes.br>
2021-03-18 13:26:14 -0300

    synced with last semester

---
+++ grade.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 grade.py Valores.txt Trajetoria.txt
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp AirSim/HelloCar/Valores.txt .
+time ./build/output/bin/HelloCar < run.txt
